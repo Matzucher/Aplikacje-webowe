aedda77366e7fed44e3d834c8b97374a411fcf14
Mateusz Zuchowski <m.zuchowski8020@gmail.com>
2026-09-24 19:55:42 +0200

    Krok 3

---
+++ mb03/api.js
@@ -0,0 +1,6 @@
+const ADRES = "https://jsonplaceholder.typicode.com/users";
+const wynik = document.querySelector("#wynik");
+const filtr = document.querySelector("#filtr");
+const licznik = document.querySelector("#licznik");
+
+let uzytkownicy = [];
