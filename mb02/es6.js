c306ca189fc5ede11dfe229cdddaa69b1868e0bc
Mateusz Zuchowski <m.zuchowski8020@gmail.com>
2026-09-24 19:45:51 +0200

    Krok 7

--- mb02/es6.js
+++ mb02/es6.js
@@ -11,5 +11,5 @@ export const nazwyAktywnych = tablica =>
 export const sumaGodzin = tablica =>
     tablica.reduce((suma, kurs) => suma + kurs.godziny, 0);
 
-export const opis = kurs => 
-    `Kurs ${kurs.nazwa} trwa ${kurs.godziny} godzin`;
+export const opis = ({ nazwa, godziny }) => 
+    `Kurs ${nazwa} trwa ${godziny} godzin`;
