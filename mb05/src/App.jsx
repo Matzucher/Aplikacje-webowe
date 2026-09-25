cc65e83c2cf75a226ba0b1839aee14e05a792fa6
matzucher <m.zuchowski8020@gmail.com>
2026-09-25 09:22:25 +0200

    mb05-Krok 3

--- mb05/src/App.jsx
+++ mb05/src/App.jsx
@@ -1,8 +1,10 @@
-function App() {
-  return (
+import Navbar from './components/Navbar.jsx'
+
+export default function App() {
+  return <>
+    <Navbar />
     <div className="container mt-4">
       <h1>Galeria zdjęć</h1>
     </div>
-  )
+  </>
 }
-export default App
